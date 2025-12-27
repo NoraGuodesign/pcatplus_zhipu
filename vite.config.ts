4162a5c76be977c94b8e2ef1f577414b9a417ee4
NoraGuodesign <2413319109@qq.com>
2025-12-28 00:58:10 +0800

    Configure GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/pcatplus_zhipu/',
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.ZHIPU_API_KEY || '3b8de6a8f8044c94a9d1d8aebb951131.katyI652XfEWrFN8'),
+        'process.env.ZHIPU_API_KEY': JSON.stringify(env.ZHIPU_API_KEY || '3b8de6a8f8044c94a9d1d8aebb951131.katyI652XfEWrFN8')
       },
       resolve: {
         alias: {
